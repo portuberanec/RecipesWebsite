888aacb97f4e311bdcc51c7ace3c04390972972c
portuberanec <portuberanec@gmail.com>
2024-06-06 22:08:13 +0500

    Создал структуру для быстрого создания новых страниц

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import { engine } from 'express-handlebars'
+import * as fs from "node:fs";
 
 const app = express()
+/*const fs = require('node:fs');*/
 
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
@@ -13,8 +15,24 @@ app.get('/', (_, res) => {
     res.render('index', {layout: 'main'})
 })
 
-app.get('/breakfast', (_, res) => {
-    res.render('breakfast', {layout: 'recipe', png1: 1})
-})
+// чтение файла со всеми страницами и передача запроса для отображения страницы
+const data = fs.readFileSync("C:/Github/RecipesWebsite/public/Webpages_info.txt", 'utf-8');
+let STR = data.toString();
+STR = STR.replace("\r\n", " - ");
+let STRArray = STR.split(" - ");
+/*STRArray.forEach(element => {
+    console.log(element);
+});*/
+
+const RetArray = STRArray;
+  //  console.log(STR.slice(0, STR.indexOf(' -')));   // выводим считанные данные
+
+
+for (let i = 0; i < STRArray.length - 1; i = i + 2) {
+    app.get(RetArray[i], (_, res) => {
+        res.render(RetArray[i+1], {layout: 'recipe', png1: 1})
+    })
+    //console.log(RetArray[i]);
+}
 
 app.listen(3000, () => console.log('Server started'))
